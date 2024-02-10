5a62d865094aaed40adc3bb66198475ba1b96c62
Raj Kamlesh Patel <rj227488@dal.ca>
2024-02-09 23:40:55 -0400

    add register logic

--- backend/src/main/java/com/rentalsphere/backend/Authentication/Service/AuthenticationService.java
+++ backend/src/main/java/com/rentalsphere/backend/Authentication/Service/AuthenticationService.java
@@ -3,8 +3,10 @@ package com.rentalsphere.backend.Authentication.Service;
 import com.rentalsphere.backend.Authentication.Service.IService.IAuthenticationService;
 import com.rentalsphere.backend.Configuration.JwtService;
 import com.rentalsphere.backend.Enums.Roles;
+import com.rentalsphere.backend.Exception.User.UserAlreadyExistsException;
 import com.rentalsphere.backend.RequestResponse.Authentication.AuthenticationRequest;
 import com.rentalsphere.backend.RequestResponse.Authentication.AuthenticationResponse;
+import com.rentalsphere.backend.Role.Repository.RoleRepository;
 import com.rentalsphere.backend.User.Model.User;
 import com.rentalsphere.backend.User.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,27 +14,37 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService implements IAuthenticationService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     @Override
     public AuthenticationResponse register(AuthenticationRequest request) {
+        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new UserAlreadyExistsException("User with same email already exists");
+        }
         User user = User.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .role(Roles.USER)
+                .roles(List.of(roleRepository.findByName(Roles.USER)))
                 .build();
         userRepository.save(user);
+        List<String> userRoles = user.getRoles().stream().map(role -> role.getName().name()).toList();
         String token = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
+                .isSuccess(true)
                 .email(user.getEmail())
                 .token(token)
+                .roles(userRoles)
+                .timeStamp(new Date())
                 .build();
     }
 }
