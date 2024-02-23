fafb5c7ed55502d796310dd3a9864fe2426d8db5
Yash <yashbest005@gmail.com>
2024-02-23 16:30:12 -0400

    feat: setup routing for manager subroute components

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -4,7 +4,13 @@ import ErrorPage from "./components/ErrorPage.jsx";
 import Register from "./components/Register.jsx";
 import Landing from "./components/Landing.jsx";
 import TenantDashboard from "./components/TenantDashboard.jsx";
-import ManagerDashboard from "./components/ManagerDashboard.jsx";
+import ManagerDashboard from "./components/ManagerDashboard/ManagerDashboard.jsx";
+import PMRentManage from "./components/ManagerDashboard/PMRentManage.jsx";
+import PMLeaseManage from "./components/ManagerDashboard/PMLeaseManage.jsx";
+import PMViolationLog from "./components/ManagerDashboard/PMViolationLog.jsx";
+import PMServiceReqs from "./components/ManagerDashboard/PMServiceReqs.jsx";
+import PMAnnouncements from "./components/ManagerDashboard/PMAnnouncements.jsx";
+import PMOverview from "./components/ManagerDashboard/PMOverview.jsx";
 
 const AppRoutes = () => {
   return (
@@ -13,7 +19,14 @@ const AppRoutes = () => {
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/register" element={<Register />} />
       <Route exact path="/tenantdashboard" element={<TenantDashboard />} />
-      <Route exact path="/managerdashboard" element={<ManagerDashboard />} />
+      <Route exact path="/managerdashboard" element={<ManagerDashboard />}>
+        <Route exact path="overview" element={<PMOverview />} />
+        <Route exact path="rentmanagement" element={<PMRentManage />} />
+        <Route exact path="leasemanagement" element={<PMLeaseManage />} />
+        <Route exact path="violationlog" element={<PMViolationLog />} />
+        <Route exact path="servicerequests" element={<PMServiceReqs />} />
+        <Route exact path="announcements" element={<PMAnnouncements />} />
+      </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
