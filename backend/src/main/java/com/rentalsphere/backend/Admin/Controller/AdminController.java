042486d072c57cbb61804559ca3b5dd566559a3a
Yash <yashbest005@gmail.com>
2024-03-10 15:32:59 -0300

    chore: fix backend errors

--- backend/src/main/java/com/rentalsphere/backend/Admin/Controller/AdminController.java
+++ backend/src/main/java/com/rentalsphere/backend/Admin/Controller/AdminController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
