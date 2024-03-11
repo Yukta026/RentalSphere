eac5ad7ac12df11fbc16641cb5397424a76a4909
Harsh Maisuri <hr786278@dal.ca>
2024-03-11 17:20:20 -0300

    feat: Rent-management component add status button

--- frontend/src/components/ManagerDashboard/PMRentManage.jsx
+++ frontend/src/components/ManagerDashboard/PMRentManage.jsx
@@ -7,6 +7,14 @@ export default function RentManage() {
 
   const [selectedOption, setSelectedOption] = useState('');
 
+  const statusClassMapping = {
+    overdue: 'text-red-800 bg-red-200',
+    paid: 'text-green-800 bg-green-200',
+    partial: 'text-yellow-800 bg-yellow-200',
+    open: 'text-black bg-gray-200',
+  };
+   
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-10">
@@ -33,26 +41,34 @@ export default function RentManage() {
         <h6 className='block pb-2 text-gray-12 text-[18px] font-semibold mt-8'>Payment</h6>
 
 
-      <div className="flex gap-10">
+      <div className="flex gap-8 justify-between">
 
-        <div className='w-[60%] mt-4'>
+        <div className='w-[70%] mt-4'>
           <div className="w-full">
             <table className='table-auto w-full h-full'>
               <thead className='text-left text-gray-4 uppercase text-[14px] tracking-wider'>
                 <tr className='border-b-2 border-gray py-10'>
-                  <th className='pb-5 px-3'>Invoice ID</th>
-                  <th className='pb-5 px-3'>Amount</th>
-                  <th className='pb-5 px-3'>Date</th>
-                  <th className='pb-5 px-3'>Note</th>
+                  <th className='pb-5 px-3 text-center capitalize'>Status</th>
+                  <th className='pb-5 px-3 text-center'>Invoice ID</th>
+                  <th className='pb-5 px-3 text-center'>Tenant</th>
+                  <th className='pb-5 px-3 text-center'>Amount</th>
+                  <th className='pb-5 px-3 text-center'>Date</th>
+                  <th className='pb-5 px-3 text-center'>Due Date</th>
+                  <th className='pb-5 px-3 text-center'>Category </th>
                 </tr>
               </thead>
               <tbody className='text-black font-gilroy-medium'>
                 {samplePaymentData.map((data, index) => (
                   <tr key={index} className='border-b-2 border-gray py-10'>
-                    <td className='py-4 px-3'>{data.invoiceNo}</td>
-                    <td className='py-4 px-3'>$ {data.amount}</td>
-                    <td className='py-4 px-3 '>{data.paymentDate}</td>
-                    <td className='py-4 px-3 '>{data.note}</td>
+                    <td className={`py-4 px-3 text-center capitalize`}>
+                      <p className={`${statusClassMapping[data.status]} rounded-full`}>{data.status}</p>
+                      </td>
+                    <td className='py-4 px-3 text-center'>{data.invoiceNo}</td>
+                    <td className='py-4 px-3 text-center'>{data.tenant}</td>
+                    <td className='py-4 px-3 text-center'>$ {data.amount}</td>
+                    <td className='py-4 px-3 text-center'>{data.paymentDate}</td>
+                    <td className='py-4 px-3 text-center'>{data.paymentDueDate}</td>
+                    <td className='py-4 px-3 text-center'>{data.note}</td>
                   </tr>
                 ))}
               </tbody>
@@ -60,7 +76,7 @@ export default function RentManage() {
           </div>
         </div>
 
-        <div className="w-[40%]">
+        <div className="w-[30%]">
           <div className="bg-white drop-shadow-md border border-gray-300 text-center py-10">
             <h6 className="font-semibold text-[16px] text-gray-600">Current balance</h6>
             <p className="font-bold text-[30px]">$ 2850.00</p>
@@ -70,38 +86,38 @@ export default function RentManage() {
             <h6 className="font-semibold text-[20px] text-gray-600">Lease Information</h6>
 
             <p className="mt-4 text-[18px]">Account number</p>
-            <p className="text-[14px]">FDF525252FF</p>
+            <p className="text-[16px]">FDF525252FF</p>
 
             <p className="mt-4 text-[18px]">Address</p>
-            <p className="text-[14px]">7 street line road<br /> Boston MA 202020 <br /> United State </p>
+            <p className="text-[16px]">7 street line road, Boston MA 202020, United State </p>
 
             <div className="flex gap-10 items-center">
-              <div>
+              <div className="w-[40%]">
                 <p className="mt-4 text-[18px]">Start date</p>
-                <p className="text-[14px]">April 2023</p>
+                <p className="text-[16px]">April 2023</p>
               </div>
 
-              <div>
+              <div className="w-[40%]">
                 <p className="mt-4 text-[18px]">End date</p>
-                <p className="text-[14px]">March 2024</p>
+                <p className="text-[16px]">March 2024</p>
               </div>
             </div>
 
             <div className="flex gap-10 items-center">
-              <div>
+              <div className="w-[40%]">
                 <p className="mt-4 text-[18px]">Rent</p>
-                <p className="text-[14px]">$ 1000</p>
+                <p className="text-[16px]">$ 1000</p>
               </div>
 
-              <div>
+              <div className="w-[40%]">
                 <p className="mt-4 text-[18px]">Maintenance </p>
-                <p className="text-[14px]">$ 200</p>
+                <p className="text-[16px]">$ 200</p>
               </div>
             </div>
 
             <div>
               <p className="mt-4 text-[18px]">Deposite </p>
-              <p className="text-[14px]">$ 1000</p>
+              <p className="text-[16px]">$ 1000</p>
             </div>
 
           </div>
