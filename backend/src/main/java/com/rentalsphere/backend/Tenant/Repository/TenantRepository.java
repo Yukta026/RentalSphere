6e0eae70b5aaa40b016ca8e22204d539aa20b19b
Raj Kamlesh Patel <rj227488@dal.ca>
2024-03-14 19:32:45 -0300

    chore:update the tenantRepository interface

--- backend/src/main/java/com/rentalsphere/backend/Tenant/Repository/TenantRepository.java
+++ backend/src/main/java/com/rentalsphere/backend/Tenant/Repository/TenantRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface TenantRepository extends JpaRepository<Tenant, Long> {
