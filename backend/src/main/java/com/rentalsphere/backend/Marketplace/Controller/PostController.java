3294e28ce3d5a00452d35ac7067840d0f6a33fc4
Raj Kamlesh Patel <rj227488@dal.ca>
2024-03-28 18:45:41 -0300

    chore:update the cross origins

--- backend/src/main/java/com/rentalsphere/backend/Marketplace/Controller/PostController.java
+++ backend/src/main/java/com/rentalsphere/backend/Marketplace/Controller/PostController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-//@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "/api/v1/marketplace/post")
 @RequiredArgsConstructor
